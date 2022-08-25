698402e102c8d65292c7402c783278828f0346f5
Teejay Villaluz <traxxjester25@gmail.com>
2022-08-26 02:20:14 +0800

    first commit

---
+++ js/portfolio2.js
@@ -0,0 +1,62 @@
+
+
+// Navigation
+
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+    hideSection();
+    toggleNavbar();
+    document.body.classList.toggle("hide-scrolling");
+});
+
+function hideSection() {
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+function toggleNavbar() {
+    document.querySelector(".header").classList.toggle("active");
+}
+
+
+// Active Section
+
+document.addEventListener('click', (e) => {
+    if(e.target.classList.contains("link-item") && e.target.change !== ""){
+        //to prevent multiple click
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else{
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active", "fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        },500);
+    }
+});
+
+
+
+// About Tabs
+
+const aboutTab = document.querySelector('.about-tabs');
+const aboutSection = document.querySelector('.about');
+
+aboutTab.addEventListener('click', (e) => {
+    if(e.target.classList.contains('tab-item') && !e.target.classList.contains('active')) {
+        aboutTab.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
+        const target = e.target.getAttribute("data-target");       
+         aboutSection.querySelector(".tab-content.active").classList.remove('active');
+         aboutSection.querySelector(target).classList.add("active");
+    }
+});
+
+
